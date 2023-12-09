289d6b4289579de7d5953a4a54d83474bde843e0
Amirani <amiraniburjanadze@gmail.com>
2023-12-09 16:50:04 +0400

    create server sidebar and header

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,15 +1,42 @@
-import NavigationSidebar from "@/components/navigation/navigation-sidebar";
-export default async function layout({
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import ServerSidebar from "@/components/server/server-sidebar";
+
+export default async function ServerIdLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { serverId: string };
 }) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
-        <NavigationSidebar />
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
       </div>
-      <main className="md:pl-[72px] h-full">{children}</main>
+      <main className="h-full md:pl-60">{children}</main>
     </div>
   );
 }
